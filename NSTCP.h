151ca2410281c33237978966ac9c1bbfa2ddd857
Mark Tripoli <mgtripoli@comcast.net>
2018-01-28 01:08:39 -0500

    Initial Commit

---
+++ NSTCP.h
@@ -0,0 +1,18 @@
+#pragma PAGE "nstcp.h"
+
+/************************************************************************************
+*
+*		AUTHOR:		Mark Tripoli
+*		DATE:		28 - FEB - 2018
+*		LICENSE:	MIT License
+*		
+*		Purpose:	This library is geared towards TCP/IP operations within
+*					NonStop Guardian enviornments. Some of the code may need to be
+*					tweaked, as this was geared towards compilation on a C++ compiler.
+*					
+*		Notes:		When utilizing this library, please bear in mind, except for 
+*					initialization, you will be operating through the TCP structure
+*					when making calls. It is similar to working with object-based code.
+*
+*
+*************************************************************************************/
